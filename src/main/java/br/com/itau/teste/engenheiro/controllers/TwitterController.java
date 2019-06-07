9266439b7f8ea5a1958ab18f9cd61ec0d3d9ba8d
borgessouza <borgessouza@gmail.com>
2019-06-07 00:07:06 -0300

    init work

---
+++ src/main/java/br/com/itau/teste/engenheiro/controllers/TwitterController.java
@@ -0,0 +1,76 @@
+package br.com.itau.teste.engenheiro.controllers;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.event.Event;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@RestController
+@RequestMapping("twitter/")
+public class TwitterController {
+
+    @GetMapping("tag")
+    public String getTwitter() {
+
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+        BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+
+        // Optional: set up some followings and track terms
+        //List<Long> followings = Lists.newArrayList(1234L, 566788L);
+        List<String> terms = Lists.newArrayList("Neymar");
+        //hosebirdEndpoint.followings(followings);
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1("", "",
+                "-", "");
+
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        // Attempts to establish a connection.
+        hosebirdClient.connect();
+
+        try {
+            // on a different thread, or multiple different threads....
+            while (!hosebirdClient.isDone()) {
+                String msg = null;
+
+                msg = msgQueue.take();
+                System.out.println(msg);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        hosebirdClient.stop();
+
+        return "Ola";
+    }
+
+}
